c42d7af7676de92f54a73912b57cdc22c5d8e149
focus110 <iamemmaochuko@gmail.com>
2022-09-20 19:09:04 +0100

    make nav responsive

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,12 +4,14 @@ import logo from "../Assets/Image/logo.svg";
 
 const Nav = () => {
   return (
-    <div className="flex justify-between container py-8 border-b">
+    <div className="flex justify-between items-end container py-8 border-b">
       <Link to="/" className="flex items-end">
         <img className="w-8 mr-4" src={logo} alt="logo" />
-        <h2 className="text-xl font-semibold">Activity Lab</h2>
+        <h2 className="text-sm sm:text-xl font-semibold">Activity Lab</h2>
       </Link>
-      <Link to="/">Add assignment</Link>
+      <Link className="text-sm sm:text-base" to="/">
+        Add assignment
+      </Link>
     </div>
   );
 };
