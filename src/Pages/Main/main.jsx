3439d2257b6b6e346c7715a585758c4e4136893d
focus110 <iamemmaochuko@gmail.com>
2022-09-20 15:33:52 +0100

    folder restructured

---
+++ src/Pages/Main/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../../component/layout/Nav";
+
+const main = () => {
+  return (
+    <div>
+      <Nav />
+      <div>main</div>
+    </div>
+  );
+};
+
+export default main;
